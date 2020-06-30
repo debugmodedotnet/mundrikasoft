025488d888fca79043f24bcf3d1a9ff11a107a2c
debugmodedotnet <debugmode@outlook.com>
2020-06-30 15:36:35 +0530

    student regiteartion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
